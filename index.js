8e649727608afb22221730f0162bb28a2f653ce9
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-08-29 18:54:41 +0900

    feat: query teams and team

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 const database = require('./database')
-console.log(database.roles.map((item) => item.requirement))
+const {
+  ApolloServer,
+  gql
+} = require('apollo-server')
+const typeDefs = gql `
+  type Query {
+    teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipments]
+    supplies: [Supplies]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+  type Equipments {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supplies {
+    id: String
+    team: Int
+  }
+`
+const resolvers = {
+  Query: {
+    teams: () => database.teams,
+    team: (parent, args, context, info) =>
+      database.teams.filter((team) => {
+        return team.id === args.id
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies
+  }
+}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+server.listen().then(({
+  url
+}) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
