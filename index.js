cc377db2ec94a4edbe9cbe2a748b9b3080c2e228
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-09-06 14:50:51 +0900

    fix: add dbWorks, make modules

--- index.js
+++ index.js
@@ -3,90 +3,23 @@ const {
   ApolloServer,
   gql
 } = require('apollo-server')
-const typeDefs = gql `
-  type Query {
-    teams: [Team]
-    team(id: Int): Team
-    equipments: [Equipment]
-    supplies: [Supply]
-  }
-  type Mutation {
-    deleteEquipment(id: String): Equipment
-    editEquipment(
-      id: String
-      used_by: String
-      count: Int
-      new_or_used: String
-    ): Equipment
-    insertEquipment(
-      id: String
-      used_by: String
-      count: Int
-      new_or_used: String
-    ): Equipment
-  }
-  type Team {
-    id: Int
-    manager: String
-    office: String
-    extension_number: String
-    mascot: String
-    cleaning_duty: String
-    project: String
-    supplies: [Supply]
-  }
-  type Equipment {
-    id: String
-    used_by: String
-    count: Int
-    new_or_used: String
-  }
-  type Supply {
-    id: String
-    team: Int
-  }
-`
-const resolvers = {
-  Query: {
-    teams: () => database.teams
-      .map((team) => {
-        team.supplies = database.supplies
-          .filter((supply) => {
-            return supply.team === team.id
-          })
-        return team
-      }),
-    team: (parent, args, context, info) =>
-      database.teams.filter((team) => {
-        return team.id === args.id
-      })[0],
-    equipments: () => database.equipments,
-    supplies: () => database.supplies
-  },
-  Mutation: {
-    deleteEquipment: (parent, args, context, info) => {
-      const deleted = database.equipments.filter((equipment) => {
-        return equipment.id === args.id
-      })[0];
-      database.equipments = database.equipments.filter((equipment) => {
-        return equipment.id !== args.id
-      });
-      return deleted;
-    },
-    insertEquipment: (parent, args, context, info) => {
-      database.equipments.push(args)
-      return args
-    },
-    editEquipment: (parent, args, context, info) => {
-      return database.equipments.filter((eq) => {
-        return eq.id === args.id
-      }).map(item => {
-        Object.assign(item, args)
-        return item
-      })[0]
-    }
-  }
-}
+
+const queries = require('./typedefs-resolvers/_queries')
+const mutations = require('./typedefs-resolvers/_mutations')
+const equipments = require('./typedefs-resolvers/equipments')
+const supplies = require('./typedefs-resolvers/supplies')
+
+const typeDefs = [
+  queries,
+  mutations,
+  equipments.typeDefs,
+  supplies.typeDefs,
+]
+const resolvers = [
+  equipments.resolvers,
+  supplies.resolvers
+]
+
 const server = new ApolloServer({
   typeDefs,
   resolvers
---
+++ typedefs-resolvers/equipments.js
@@ -0,0 +1,27 @@
+const {
+  gql
+} = require('apollo-server')
+const dbWorks = require('../dbWorks')
+
+const typeDefs = gql `
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    equipments: (parent, args) => dbWorks.getEquipments(args)
+  },
+  Mutation: {
+    deleteEquipment: (parent, args, context, info) => dbWorks.deleteItem('equipments', args),
+  }
+}
+
+module.exports = {
+  typeDefs: typeDefs,
+  resolvers: resolvers
+}
